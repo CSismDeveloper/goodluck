e4540b76bb2c782a5f14acb5ac396447e4f3ce39
CSismDeveloper <csismdeveloper@gmail.com>
2025-01-03 18:25:03 +0530

    new

--- db_connection.php
+++ db_connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "markeemx_samudrawavesuser"; 
 $password = "^-wb@8{1?6qT"; 
-$dbname = "goodluck_db";
+$dbname = "markeemx_goodluck_db";
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
